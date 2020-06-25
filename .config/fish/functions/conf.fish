16bc1423beac0663d291faa8862c16f1c222c751
Sly <khmedj@gmail.com>
2020-06-25 19:30:39 +0100

    Add conf alias for fish

---
+++ .config/fish/functions/conf.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function conf --wraps='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME' --description 'alias conf=/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+  /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $argv;
+end
