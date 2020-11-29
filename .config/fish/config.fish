db6c82ee778e9f7520d462a907bfd382bcd1792c
Sly <khmedj@gmail.com>
2020-11-30 00:33:10 +0100

    Update: Remove unused vars

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,11 +2,8 @@ set -g fish_user_paths "/usr/local/opt/gettext/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/openssl@1.1/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/bin" $fish_user_paths
 
-set -g -x PYTHON_HOST_PROG "/usr/bin/python2.7"
-set -g -x PYTHON3_HOST_PROG "/usr/local/bin/python3"
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/icu4c/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/icu4c/sbin" $fish_user_paths
-set -g fish_user_paths "/usr/local/opt/python@3.8/bin" $fish_user_paths
 
 starship init fish | source
