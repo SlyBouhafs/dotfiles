65d29fbee8564a8417127f76f102576faa5ac0a6
Sly <khmedj@gmail.com>
2021-10-28 16:39:32 +0100

    Updated config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,7 @@
-set -g fish_user_paths "/usr/local/opt/gettext/bin" $fish_user_paths
-set -g fish_user_paths "/usr/local/opt/openssl@1.1/bin" $fish_user_paths
-set -g fish_user_paths "/usr/local/bin" $fish_user_paths
+fish_add_path /usr/local/sbin
+fish_add_path /usr/local/opt/gettext/bin
+fish_add_path /usr/local/opt/openssl@1.1/bin
+fish_add_path /usr/local/bin
+fish_add_path /usr/local/opt/icu4c/bin
 
-set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
-set -g fish_user_paths "/usr/local/opt/icu4c/bin" $fish_user_paths
-set -g fish_user_paths "/usr/local/opt/icu4c/sbin" $fish_user_paths
-
-starship init fish | source
+source ("/usr/local/bin/starship" init fish --print-full-init | psub)
